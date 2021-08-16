58224d238d9d81467478dec0294d9dc9904eabda
Guilherme Govaski <guigovaski@hotmail.com>
2021-08-16 17:31:43 -0300

    Removendo a parte subscribe e ajustando o codigo

--- core/views.py
+++ core/views.py
@@ -22,10 +22,12 @@ class IndexView(FormView):
         context['fotos'] = Slider.objects.all()
         return context
 
+
     def form_valid(self, form, *args, **kwargs):
         form.send_mail()
         messages.success(self.request, 'E-mail enviado com sucesso')
         return super(IndexView, self).form_valid(form, *args, **kwargs)
+
 
     def form_invalid(self, form, *args, **kwargs):
         messages.error(self.request, 'Erro ao enviar e-mail')
