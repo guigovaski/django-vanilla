9e10614939f722c20be72314fa3eedfb60b71b03
Guilherme Govaski <guigovaski@hotmail.com>
2021-09-08 23:37:48 -0300

    Ajustes no admin theme

--- vanilla/urls.py
+++ vanilla/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# admin skin
+# admin theme
 admin.AdminSite.site_header = 'Administração Vanilla'
 admin.AdminSite.site_title = 'Vanilla'
 admin.AdminSite.index_title = 'Sistema Vanilla'
